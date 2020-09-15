b7d58f66ab88a646e064e8cd97d23f568e44b611
Nikolas Bertelshofer <etc.nikolas@gmail.com>
2020-09-15 19:10:03 +0200

    Added first labels and title

--- src/main/java/TheMovieDB/MainApp.java
+++ src/main/java/TheMovieDB/MainApp.java
@@ -3,8 +3,18 @@ import info.movito.themoviedbapi.TmdbApi;
 import info.movito.themoviedbapi.TmdbMovies;
 import info.movito.themoviedbapi.model.MovieDb;
 import javafx.application.Application;
+import javafx.geometry.HPos;
+import javafx.geometry.Insets;
+import javafx.geometry.Pos;
+import javafx.geometry.VPos;
 import javafx.scene.Group;
 import javafx.scene.Scene;
+import javafx.scene.control.Label;
+import javafx.scene.effect.DropShadow;
+import javafx.scene.layout.*;
+import javafx.scene.paint.Color;
+import javafx.scene.text.Font;
+import javafx.scene.text.FontWeight;
 import javafx.stage.Stage;
 import org.apache.log4j.BasicConfigurator;
 
@@ -24,17 +34,38 @@ public class MainApp extends Application {
 
     @Override
     public void start(Stage ir_primaryStage) throws Exception {
-        Group lr_root = new Group();
-        Scene lr_scene = new Scene(lr_root,400,300);
+        //Layout and Scene
+        VBox lr_vBox = new VBox();
+        lr_vBox.setPadding(new Insets(10,10,10,25));
+        VBox lr_vBoxTitle = new VBox();
+        lr_vBoxTitle.setPadding(new Insets(5,10,30,128));
 
-        ir_primaryStage.setTitle("Main Menu");
-        ir_primaryStage.setScene(lr_scene);
-        ir_primaryStage.show();
+        Scene lr_scene = new Scene(lr_vBox,400,300);
+
+        //Labels for showing path
+        Label lr_labelTitle = new Label ("Settings");
+        lr_labelTitle.setFont(Font.font("Verdana", FontWeight.BOLD, 20));
+        DropShadow lr_dropShadow = new DropShadow();
+        lr_dropShadow.setOffsetY(3.0f);
+        lr_dropShadow.setColor(Color.color(0.4f, 0.4f, 0.4f));
+        lr_labelTitle.setEffect(lr_dropShadow);
+
+        Label lr_labelCurrent = new Label ("Current path:");
+        lr_labelCurrent.setBackground(new Background(new BackgroundFill(Color.GOLD,null,null)));
+        Label lr_labelPath = new Label ("Actual path");
+        lr_vBoxTitle.getChildren().add(lr_labelTitle);
+        lr_vBox.getChildren().addAll(lr_vBoxTitle,lr_labelCurrent,lr_labelPath);
 
 
 
         TmdbMovies movies = new TmdbApi(readFromConfig("[API Key]")).getMovies();
         MovieDb movie = movies.getMovie(5353,"en");
+
+        //Set up and start
+        ir_primaryStage.setTitle("Main Menu");
+        ir_primaryStage.setScene(lr_scene);
+        ir_primaryStage.setResizable(false);
+        ir_primaryStage.show();
     }
 
     public String readFromConfig (String iv_keyWord) throws FileNotFoundException {
