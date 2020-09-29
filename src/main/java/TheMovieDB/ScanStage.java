6517a7daa36f5e6918243dd5d5f39441b8e35c22
Nikolas Bertelshofer <etc.nikolas@gmail.com>
2020-09-29 17:27:11 +0200

    Added nodes in second window

--- src/main/java/TheMovieDB/ScanStage.java
+++ src/main/java/TheMovieDB/ScanStage.java
@@ -9,6 +9,7 @@ import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.*;
+import javafx.scene.text.Font;
 import javafx.stage.Stage;
 
 public class ScanStage {
@@ -19,10 +20,10 @@ public class ScanStage {
     //Elements of second window
     Stage mr_stageScan;
     Scene mr_sceneScan;
-    BorderPane mr_borderPaneMain, mr_borderPaneRight;
-    Label mr_labelFolder, mr_labelFile, mr_labelSpacing;
-    VBox mr_vBoxLeft, mr_vBoxRightMid;
-    HBox mr_hBoxRightBottom;
+    BorderPane mr_borderPaneMain, mr_borderPaneRight, mr_borderPaneLeft;
+    Label mr_labelFolder, mr_labelFile, mr_labelSpacing, mr_labelMovieTitle,mr_labelSuggestion;
+    VBox mr_vBoxRightMid, mr_vBoxLeftTop;
+    HBox mr_hBoxRightBottom, mr_hBoxLeftBottom;
     Button mr_buttonManSearch, mr_buttonBack, mr_buttonForward, mr_buttonConfirm;
 
 
@@ -31,39 +32,66 @@ public class ScanStage {
         mr_buttonRoot = ir_buttonScan;
 
         //Preparation for Stage
-        mr_stageScan = new Stage();
-        mr_borderPaneMain = new BorderPane();
-        mr_borderPaneRight = new BorderPane();
         mr_hBoxRightBottom = new HBox();
-        mr_vBoxRightMid = new VBox();
-        mr_vBoxRightMid.setPadding(new Insets(30,10,0,10));
         mr_hBoxRightBottom.setSpacing(10);
         mr_hBoxRightBottom.setPadding(new Insets(5,10,10,5));
-        mr_vBoxLeft = new VBox();
-        mr_sceneScan = new Scene(mr_borderPaneMain,480,290);
-        mr_borderPaneMain.setLeft(mr_vBoxLeft);
-        mr_borderPaneMain.setRight(mr_borderPaneRight);
+
+        mr_hBoxLeftBottom = new HBox();
+        mr_hBoxLeftBottom.setSpacing(10);
+        mr_hBoxLeftBottom.setPadding(new Insets(5,10,10,70));
+
+        mr_vBoxRightMid = new VBox();
+        mr_vBoxRightMid.setPadding(new Insets(30,10,0,10));
+
+        mr_vBoxLeftTop = new VBox();
+        mr_vBoxLeftTop.setPadding(new Insets(10,0,10,85));
+        mr_vBoxLeftTop.setSpacing(5);
+
+        mr_borderPaneLeft = new BorderPane();
+        mr_borderPaneLeft.setBottom(mr_hBoxLeftBottom);
+        mr_borderPaneLeft.setTop(mr_vBoxLeftTop);
+        mr_borderPaneLeft.setPrefWidth(240);
+
+        mr_borderPaneRight = new BorderPane();
         mr_borderPaneRight.setBottom(mr_hBoxRightBottom);
         mr_borderPaneRight.setCenter(mr_vBoxRightMid);
+        mr_borderPaneRight.setPrefWidth(240);
+
+        mr_borderPaneMain = new BorderPane();
+        mr_borderPaneMain.setLeft(mr_borderPaneLeft);
+        mr_borderPaneMain.setRight(mr_borderPaneRight);
+
+        mr_stageScan = new Stage();
+        mr_sceneScan = new Scene(mr_borderPaneMain,480,290);
 
         //Configuration Buttons
         mr_buttonManSearch = new Button("Manual Search");
         mr_buttonBack = new Button("<");
         mr_buttonForward = new Button(">");
-        mr_buttonConfirm = new Button("Confirm");
+        Image lr_imageCheckmarkIcon = new Image("file:CheckMark.png",20,20,false,true);
+        mr_buttonConfirm = new Button("",new ImageView(lr_imageCheckmarkIcon));
 
         //Configuration Labels
-        Image lr_imageFolderIcon = new Image("file:FolderIcon.png",30,30,false,true);
-        mr_labelFolder = new Label ("hey",new ImageView(lr_imageFolderIcon));
-        Image lr_imageFileIcon = new Image("file:FileIcon.png",30,30,false,true);
-        mr_labelFile = new Label ("hey",new ImageView(lr_imageFileIcon));
+        mr_labelSuggestion = new Label("Suggestion:");
+        mr_labelSuggestion.setFont(new Font("Arial",15));
+        mr_labelSuggestion.setStyle("-fx-font-weight: bold;-fx-border-color: black, transparent, black; -fx-border-width: 0 0 1 0, 0 0 1 0, 0 0 1 0; -fx-border-insets: 0 0 1 0, 0 0 2 0, 0 0 3 0");
+        mr_labelMovieTitle = new Label("Movie Title");
+        mr_labelMovieTitle.setStyle("-fx-font-weight: bold;-fx-background-color: #cd5c5c");
+        mr_labelMovieTitle.setFont(new Font("Arial",15));
+        Image lr_imageFolderIcon = new Image("file:FolderIcon.png",20,20,false,true);
+        mr_labelFolder = new Label ("Current Folder ",new ImageView(lr_imageFolderIcon));
+        mr_labelFolder.setStyle("-fx-font-weight: bold;-fx-background-color: abc;-fx-border-color: black, transparent, black; -fx-border-width: 0 0 1 0, 0 0 1 0, 0 0 1 0; -fx-border-insets: 0 0 1 0, 0 0 2 0, 0 0 3 0");
+        Image lr_imageFileIcon = new Image("file:FileIcon.png",20,20,false,true);
+        mr_labelFile = new Label ("File in Folder ",new ImageView(lr_imageFileIcon));
+        mr_labelFile.setStyle("-fx-font-weight: bold;-fx-background-color: abc;-fx-border-color: black, transparent, black; -fx-border-width: 0 0 1 0, 0 0 1 0, 0 0 1 0; -fx-border-insets: 0 0 1 0, 0 0 2 0, 0 0 3 0");
         mr_labelSpacing = new Label (" \n ");
 
-
-
         //Add elements
-        mr_hBoxRightBottom.getChildren().addAll(mr_buttonManSearch,mr_buttonBack,mr_buttonForward,mr_buttonConfirm);
+        mr_hBoxRightBottom.getChildren().addAll(mr_buttonManSearch);
         mr_vBoxRightMid.getChildren().addAll(mr_labelFolder,mr_labelSpacing, mr_labelFile);
+        mr_hBoxLeftBottom.getChildren().addAll(mr_buttonBack,mr_buttonForward,mr_buttonConfirm);
+        mr_vBoxLeftTop.getChildren().addAll(mr_labelSuggestion,mr_labelMovieTitle);
+
     }
 
     public void setRootHandler () {
