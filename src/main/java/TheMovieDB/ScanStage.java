8a0eab2a54d6ed7cc773c93528a784632b860557
Nikolas Bertelshofer <etc.nikolas@gmail.com>
2020-10-01 22:41:10 +0200

    Styled second window

--- src/main/java/TheMovieDB/ScanStage.java
+++ src/main/java/TheMovieDB/ScanStage.java
@@ -21,10 +21,11 @@ public class ScanStage {
     Stage mr_stageScan;
     Scene mr_sceneScan;
     BorderPane mr_borderPaneMain, mr_borderPaneRight, mr_borderPaneLeft;
-    Label mr_labelFolder, mr_labelFile, mr_labelSpacing, mr_labelMovieTitle,mr_labelSuggestion;
-    VBox mr_vBoxRightMid, mr_vBoxLeftTop;
-    HBox mr_hBoxRightBottom, mr_hBoxLeftBottom;
+    Label mr_labelFolder, mr_labelFile, mr_labelSpacing, mr_labelMovieTitle,mr_labelSuggestion, mr_labelFolderName, mr_labelContentName, mr_labelMoviePoster;
+    VBox mr_vBoxRightMid, mr_vBoxLeftMid;
+    HBox mr_hBoxRightBottom, mr_hBoxLeftBottom, mr_hBoxLeftTop;
     Button mr_buttonManSearch, mr_buttonBack, mr_buttonForward, mr_buttonConfirm;
+    Image mr_imageMovie;
 
 
     ScanStage (Stage ir_stageRoot, Button ir_buttonScan) {
@@ -34,23 +35,27 @@ public class ScanStage {
         //Preparation for Stage
         mr_hBoxRightBottom = new HBox();
         mr_hBoxRightBottom.setSpacing(10);
-        mr_hBoxRightBottom.setPadding(new Insets(5,10,10,5));
+        mr_hBoxRightBottom.setPadding(new Insets(5,10,55,20));
 
         mr_hBoxLeftBottom = new HBox();
         mr_hBoxLeftBottom.setSpacing(10);
-        mr_hBoxLeftBottom.setPadding(new Insets(5,10,10,70));
+        mr_hBoxLeftBottom.setPadding(new Insets(5,10,50,40));
 
         mr_vBoxRightMid = new VBox();
-        mr_vBoxRightMid.setPadding(new Insets(30,10,0,10));
+        mr_vBoxRightMid.setPadding(new Insets(64,10,0,10));
 
-        mr_vBoxLeftTop = new VBox();
-        mr_vBoxLeftTop.setPadding(new Insets(10,0,10,85));
-        mr_vBoxLeftTop.setSpacing(5);
+        mr_vBoxLeftMid = new VBox();
+        mr_vBoxLeftMid.setPadding(new Insets(10,0,10,5));
+        mr_vBoxLeftMid.setSpacing(1);
+
+        mr_hBoxLeftTop = new HBox();
+        mr_hBoxLeftTop.setPadding(new Insets(10,5,5,55));
 
         mr_borderPaneLeft = new BorderPane();
         mr_borderPaneLeft.setBottom(mr_hBoxLeftBottom);
-        mr_borderPaneLeft.setTop(mr_vBoxLeftTop);
-        mr_borderPaneLeft.setPrefWidth(240);
+        mr_borderPaneLeft.setCenter(mr_vBoxLeftMid);
+        mr_borderPaneLeft.setTop(mr_hBoxLeftTop);
+        mr_borderPaneLeft.setPrefWidth(200);
 
         mr_borderPaneRight = new BorderPane();
         mr_borderPaneRight.setBottom(mr_hBoxRightBottom);
@@ -62,7 +67,7 @@ public class ScanStage {
         mr_borderPaneMain.setRight(mr_borderPaneRight);
 
         mr_stageScan = new Stage();
-        mr_sceneScan = new Scene(mr_borderPaneMain,480,290);
+        mr_sceneScan = new Scene(mr_borderPaneMain,400 ,400);
 
         //Configuration Buttons
         mr_buttonManSearch = new Button("Manual Search");
@@ -72,6 +77,8 @@ public class ScanStage {
         mr_buttonConfirm = new Button("",new ImageView(lr_imageCheckmarkIcon));
 
         //Configuration Labels
+        mr_labelFolderName = new Label("Test");
+        mr_labelContentName = new Label("Test");
         mr_labelSuggestion = new Label("Suggestion:");
         mr_labelSuggestion.setFont(new Font("Arial",15));
         mr_labelSuggestion.setStyle("-fx-font-weight: bold;-fx-border-color: black, transparent, black; -fx-border-width: 0 0 1 0, 0 0 1 0, 0 0 1 0; -fx-border-insets: 0 0 1 0, 0 0 2 0, 0 0 3 0");
@@ -85,12 +92,15 @@ public class ScanStage {
         mr_labelFile = new Label ("File in Folder ",new ImageView(lr_imageFileIcon));
         mr_labelFile.setStyle("-fx-font-weight: bold;-fx-background-color: abc;-fx-border-color: black, transparent, black; -fx-border-width: 0 0 1 0, 0 0 1 0, 0 0 1 0; -fx-border-insets: 0 0 1 0, 0 0 2 0, 0 0 3 0");
         mr_labelSpacing = new Label (" \n ");
+        mr_imageMovie = new Image("file:te.jpg",180,250,false,true);
+        mr_labelMoviePoster = new Label("",new ImageView(mr_imageMovie));
 
         //Add elements
         mr_hBoxRightBottom.getChildren().addAll(mr_buttonManSearch);
-        mr_vBoxRightMid.getChildren().addAll(mr_labelFolder,mr_labelSpacing, mr_labelFile);
+        mr_vBoxRightMid.getChildren().addAll(mr_labelFolder,mr_labelFolderName,mr_labelSpacing, mr_labelFile,mr_labelContentName);
         mr_hBoxLeftBottom.getChildren().addAll(mr_buttonBack,mr_buttonForward,mr_buttonConfirm);
-        mr_vBoxLeftTop.getChildren().addAll(mr_labelSuggestion,mr_labelMovieTitle);
+        mr_vBoxLeftMid.getChildren().addAll(mr_labelMovieTitle,mr_labelMoviePoster);
+        mr_hBoxLeftTop.getChildren().addAll(mr_labelSuggestion);
 
     }
 
@@ -98,8 +108,8 @@ public class ScanStage {
         mr_buttonRoot.setOnMouseClicked(new EventHandler<MouseEvent>() {
             @Override
             public void handle(MouseEvent event) {
-                mr_stageScan.setWidth(500);
-                mr_stageScan.setHeight(300);
+                mr_stageScan.setWidth(400);
+                mr_stageScan.setHeight(400);
                 mr_stageScan.setTitle("Scanner");
                 mr_stageScan.setScene(mr_sceneScan);
                 mr_stageScan.setResizable(false);
